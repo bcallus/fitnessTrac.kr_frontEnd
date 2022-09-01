62ef417b5d77f747e75a30606edbc212cd125b94
Brittany <brittanylcallus@gmail.com>
2022-08-31 22:53:54 -0400

    registerUser api added

--- fitness-tracker/src/Api/index.js
+++ fitness-tracker/src/Api/index.js
@@ -1,6 +1,29 @@
 
 export const BASE_URL = "http://fitnesstrac-kr.herokuapp.com/api";
 
+export async function registerUser({ username, password }) {
+    try {
+      return fetch(`${BASE_URL}/users/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: {
+            username: username,
+            password: password,
+          },
+        }),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          return result;
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
 export async function logInUser({ username, password }) {
     try {
       return fetch(`${BASE_URL}/users/login`, {
