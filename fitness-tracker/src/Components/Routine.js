42c8e5e372d84088b5759bf8e91591c2a571f4c3
Brittany <brittanylcallus@gmail.com>
2022-09-03 09:37:58 -0400

    activites rendered to individual routines on routines page

--- fitness-tracker/src/Components/Routine.js
+++ fitness-tracker/src/Components/Routine.js
@@ -1,13 +1,22 @@
 import React from "react";
+import Activity from "./Activity";
 
 const Routine = ({routine}) => {
-    // console.log(activity)
+    // console.log("routine.activities in Routine-->", routine.activities)
+
     return (
         <div className="routine">
             <p><b>Routine #{routine.id}</b></p>
             <p>Name: {routine.name}</p>
             <p>Goal: {routine.goal}</p>
             <p>Creator Name: {routine.creatorName}</p>
+            {routine.activities ? routine.activities.map(activity => (
+        <div className="activity-in-routine">
+            <p><b>Activity #{activity.id}</b></p>
+            <p>Name: {activity.name}</p>
+            <p>Description: {activity.description}</p>
+        </div>
+    )) : null}
         </div>
     )
 }
